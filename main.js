8dd43d9ed438b8459d87f2050cdf0d88032069e9
Hans Vos <hans.vos@codeimmersives.com>
2019-12-14 15:10:09 -0500

    added pokemon apis

---
+++ main.js
@@ -0,0 +1,27 @@
+for(let i = 1; i < 400; i++) {
+    const fetchPokemon = () => {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        fetch(url)
+        .then((res)=> {
+            return res.json();
+        })
+        .then ((data)=> {
+            console.log(data);
+            const pokemon = {
+             name: data.name,
+             id: data.id,
+             image: data.sprites['front_default'],
+             type: data.types
+                .map((type)=> type.type.name)
+                .join(', ')
+    
+            };
+            
+            
+            console.log(pokemon);
+        });
+    }
+    
+    fetchPokemon();
+
+}
