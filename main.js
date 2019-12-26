9d5584c5924951ff912c37a893d4aeb0418273b6
Hans Vos <hans.vos@codeimmersives.com>
2019-12-26 16:21:51 -0500

    50

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ const filterInputFunctionType = () => {
 
 const fetchPokemon = () => {
     const promises = [];
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= 50; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         promises.push(fetch(url).then((res) => res.json()));
     }
